c1b498cc49702e4edd41f1f74b5fbd5748f901ac
github username <ianjpollack@gmail.com>
2018-05-01 16:31:07 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,14 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		raise params.inspect
+	end
+
 	def new
 		@post = Post.new
 	end
